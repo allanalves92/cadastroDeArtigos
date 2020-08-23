5886fae6ec086e91f318a0cfd13e2fa99b787871
Allan Alves <allanalvesmail@gmail.com>
2020-08-23 16:39:56 -0300

    first commit

---
+++ backend/config/mongodb.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/knowledge_stats', {
+    useNewUrlParser: true,
+  })
+  .catch((e) => {
+    const msg = 'ERRO! Não foi possível conectar com o Mongodb';
+    console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m');
+  });
---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="footer">
+    <span>
+      Copyright
+      <strong>Aplicação para estudos Vue JS</strong> @ 2020-2021
+    </span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style>
+.footer {
+  grid-area: footer;
+  background-color: #ddd;
+  color: #333;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding-right: 10px;
+}
+</style>
